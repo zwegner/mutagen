4664b7263f76275b25f7819700701a0eeaa0f5a9
Zach Wegner <zwegner@gmail.com>
2013-05-05 12:56:22 -0500

    Get rid of everything-is-an-expr for now, it was making parsing weird.

--- bootstrap/parse.py
+++ bootstrap/parse.py
@@ -34,20 +34,24 @@ def p_error(p):
     sys.exit(1)
 
 def p_stmt_list(p):
-    """ stmt_list : expr """
+    """ stmt_list : stmt """
     p[0] = [p[1]]
 
 def p_stmt_list_2(p):
-    """ stmt_list : stmt_list expr """
-    p[0] = p[1] + [p[2]]
+    """ stmt_list : delim """
+    p[0] = []
 
 def p_stmt_list_3(p):
-    """ stmt_list : stmt_list delim """
+    """ stmt_list : stmt_list delim
+                  | stmt_list stmt
+    """
     p[0] = p[1]
+    if p[2] is not None:
+        p[0].append(p[2])
 
 def p_pass(p):
-    """ stmt_list : PASS """
-    p[0] = []
+    """ pass : PASS """
+    p[0] = None
 
 def p_block(p):
     """ block : COLON delim INDENT stmt_list DEDENT
@@ -65,13 +69,25 @@ def p_delim(p):
     p[0] = None
 
 def p_import(p):
-    """ expr : IMPORT IDENTIFIER
-             | FROM IDENTIFIER IMPORT STAR
+    """ import : IMPORT IDENTIFIER
+               | FROM IDENTIFIER IMPORT STAR
     """
     if len(p) == 3:
         p[0] = syntax.Import(p[2], None)
     else:
         p[0] = syntax.Import(p[2], [])
+
+def p_stmt(p):
+    """ stmt : expr delim
+             | pass
+             | import
+             | if_stmt
+             | assn
+             | def_stmt
+             | for_stmt
+             | while_stmt
+    """
+    p[0] = p[1]
 
 def p_expr_list(p):
     """ expr_list : expr
@@ -83,25 +99,34 @@ def p_expr_list(p):
         p[0] = p[1] + [p[3]]
 
 def p_expr(p):
-    """ expr : LPAREN expr RPAREN
-             | list
-             | def
+    """ expr : list
+             | lambda
+             | ident
+             | string
+             | integer
+             | unop
+             | binop
+             | getattr
+             | getitem
+             | call
+             | nil
     """
-    if len(p) == 4:
-        p[0] = p[2]
-    else:
-        p[0] = p[1]
+    p[0] = p[1]
+
+def p_paren_expr(p):
+    """ expr : LPAREN expr RPAREN """
+    p[0] = p[2]
 
 def p_ident(p):
-    """ expr : IDENTIFIER """
+    """ ident : IDENTIFIER """
     p[0] = syntax.Identifier(p[1])
 
 def p_string(p):
-    """ expr : STRING """
+    """ string : STRING """
     p[0] = syntax.String(p[1])
 
 def p_integer(p):
-    """ expr : INTEGER """
+    """ integer : INTEGER """
     p[0] = syntax.Integer(p[1])
 
 def p_list(p):
@@ -114,36 +139,36 @@ def p_list(p):
         p[0] = syntax.List(p[2])
 
 def p_nil(p):
-    """ expr : NIL """
+    """ nil : NIL """
     p[0] = syntax.Nil()
 
 def p_unary_op(p):
-    """ expr : NOT expr """
+    """ unop : NOT expr """
     p[0] = syntax.UnaryOp(p[1], p[2])
 
 def p_binary_op(p):
-    """ expr : expr EQUALS_EQUALS expr
-             | expr NOT_EQUALS expr
-             | expr GREATER expr
-             | expr GREATER_EQUALS expr
-             | expr LESS expr
-             | expr LESS_EQUALS expr
-             | expr PLUS expr
-             | expr AND expr
-             | expr OR expr
+    """ binop : expr EQUALS_EQUALS expr
+              | expr NOT_EQUALS expr
+              | expr GREATER expr
+              | expr GREATER_EQUALS expr
+              | expr LESS expr
+              | expr LESS_EQUALS expr
+              | expr PLUS expr
+              | expr AND expr
+              | expr OR expr
     """
     p[0] = syntax.BinaryOp(p[2], p[1], p[3])
 
 def p_getattr(p):
-    """ expr : expr PERIOD IDENTIFIER """
+    """ getattr : expr PERIOD IDENTIFIER """
     p[0] = syntax.GetAttr(p[1], p[3])
 
 def p_getitem(p):
-    """ expr : expr LBRACKET expr RBRACKET """
+    """ getitem : expr LBRACKET expr RBRACKET """
     p[0] = syntax.GetItem(p[1], p[3])
 
 def p_call(p):
-    """ expr : expr LPAREN expr_list RPAREN
+    """ call : expr LPAREN expr_list RPAREN
              | expr LPAREN RPAREN
     """
     if len(p) == 4:
@@ -152,16 +177,16 @@ def p_call(p):
         p[0] = syntax.Call(p[1], p[3])
 
 def p_assignment(p):
-    """ expr : IDENTIFIER EQUALS expr """
+    """ assn : IDENTIFIER EQUALS expr """
     p[0] = syntax.Assignment(p[1], p[3])
 
 def p_if(p):
-    """ expr : IF expr block """
+    """ if_stmt : IF expr block """
     p[0] = syntax.IfElse(p[2], p[3], [])
 
 def p_ifelse(p):
-    """ expr : IF expr block elif_stmt
-             | IF expr block else_stmt
+    """ if_stmt : IF expr block elif_stmt
+                | IF expr block else_stmt
     """
     p[0] = syntax.IfElse(p[2], p[3], p[4])
 
@@ -180,11 +205,11 @@ def p_else(p):
     p[0] = p[2]
 
 def p_for(p):
-    """ expr : FOR IDENTIFIER IN expr block """
+    """ for_stmt : FOR IDENTIFIER IN expr block """
     p[0] = syntax.For(p[2], p[4], p[5])
 
 def p_while(p):
-    """ expr : WHILE expr block """
+    """ while_stmt : WHILE expr block """
     p[0] = syntax.While(p[2], p[3])
 
 def p_arg_list(p):
@@ -206,11 +231,11 @@ def p_args(p):
         p[0] = p[2]
 
 def p_def(p):
-    """ def : DEF IDENTIFIER args block """
+    """ def_stmt : DEF IDENTIFIER args block """
     p[0] = syntax.Assignment(p[2], syntax.Function(p[2], p[3], p[4]))
 
 def p_lambda(p):
-    """ def : LAMBDA args block """
+    """ lambda : LAMBDA args block """
     p[0] = syntax.Function('lambda', p[2], p[3])
 
 parser = yacc.yacc()
