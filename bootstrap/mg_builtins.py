f7b3b4e2f4b02bc60b56b5624cb28607e7a5a0eb
Zach Wegner <zwegner@w2odigital.com>
2013-05-23 12:28:05 -0500

    Add generic type checking/arg unpacking to bootstrap builtins

--- bootstrap/mg_builtins.py
+++ bootstrap/mg_builtins.py
@@ -5,70 +5,67 @@ import syntax
 builtins = {}
 
 builtin_info = syntax.Info('__builtins__', 0)
-def mg_builtin(name):
+def mg_builtin(name, arg_types):
     def annotate(fn):
-        builtins[name] = syntax.BuiltinFunction(name, fn, info=builtin_info)
-        return fn
+        def builtin_call(ctx, args):
+            if arg_types is not None:
+                assert len(args) == len(arg_types)
+                for a, t in zip(args, arg_types):
+                    assert isinstance(a, t)
+            return fn(ctx, *args)
+
+        builtins[name] = syntax.BuiltinFunction(name, builtin_call, info=builtin_info)
+        return builtin_call
     return annotate
 
 # TODO: error checking!
 
-@mg_builtin('read_file')
-def mgb_read_file(ctx, args):
-    path, = args
+@mg_builtin('read_file', [syntax.String])
+def mgb_read_file(ctx, path):
     with open(path.value) as f:
         return syntax.String(f.read(), info=builtin_info)
 
-@mg_builtin('putchar')
-def mgb_putchar(ctx, args):
-    sys.stdout.write(args[0].value)
+@mg_builtin('putchar', [syntax.String])
+def mgb_putchar(ctx, arg):
+    sys.stdout.write(arg.value)
     return syntax.Nil(info=builtin_info)
 
-@mg_builtin('len')
-def mgb_len(ctx, args):
-    return syntax.Integer(len(args[0]), info=builtin_info)
+@mg_builtin('len', [syntax.Node])
+def mgb_len(ctx, arg):
+    return syntax.Integer(len(arg), info=builtin_info)
 
-@mg_builtin('repr')
-def mgb_repr(ctx, args):
-    arg, = args
+@mg_builtin('repr', [syntax.Node])
+def mgb_repr(ctx, arg):
     return syntax.String(repr(arg), info=builtin_info)
 
-@mg_builtin('str')
-def mgb_str(ctx, args):
-    arg, = args
+@mg_builtin('str', [syntax.Node])
+def mgb_str(ctx, arg):
     return syntax.String(str(arg), info=builtin_info)
 
-@mg_builtin('make')
-def mgb_make(ctx, args):
-    return syntax.Object(args, info=builtin_info)
+@mg_builtin('make', None)
+def mgb_make(ctx, *args):
+    return syntax.Object(list(args), info=builtin_info)
 
-@mg_builtin('reduce')
-def mgb_reduce(ctx, args):
-    fn, start, iter = args
+@mg_builtin('reduce', [syntax.Function, syntax.Node, syntax.Node])
+def mgb_reduce(ctx, fn, start, iter):
     for i in iter:
         start = fn.eval_call(ctx, [start, i.eval(ctx)])
     return start
 
-@mg_builtin('slice')
-def mgb_slice(ctx, args):
-    seq, start, end = args
+@mg_builtin('slice', [syntax.Node, syntax.Integer, syntax.Integer])
+def mgb_slice(ctx, seq, start, end):
     if isinstance(seq, syntax.String):
         return syntax.String(seq.value[start.value:end.value], info=builtin_info)
     elif isinstance(seq, syntax.List):
         return syntax.List(seq.items[start.value:end.value], info=builtin_info)
     return syntax.Nil(info=builtin_info)
 
-@mg_builtin('parse_int')
-def mgb_parse_int(ctx, args):
-    int_str, base = args
-    assert (isinstance(int_str, syntax.String) and
-            isinstance(base, syntax.Integer))
+@mg_builtin('parse_int', [syntax.String, syntax.Integer])
+def mgb_parse_int(ctx, int_str, base):
     return syntax.Integer(int(int_str.value, base.value), info=builtin_info)
 
-@mg_builtin('str_upper')
-def mgb_str_upper(ctx, args):
-    arg, = args
-    assert isinstance(arg, syntax.String)
+@mg_builtin('str_upper', [syntax.String])
+def mgb_str_upper(ctx, arg):
     return syntax.String(arg.value.upper(), info=builtin_info)
 
 __all__ = builtins
