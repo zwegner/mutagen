3a127b540a1cab1851853d71af29f658fef1dc60
Zach Wegner <zwegner@w2odigital.com>
2013-06-05 19:05:21 -0500

    Get builtin name from function name in mg_builtins

--- bootstrap/mg_builtins.py
+++ bootstrap/mg_builtins.py
@@ -4,7 +4,7 @@ from syntax import *
 
 builtins = {}
 
-def mg_builtin(name, arg_types):
+def mg_builtin(arg_types):
     def annotate(fn):
         def builtin_call(obj, ctx, args):
             if arg_types is not None:
@@ -17,39 +17,40 @@ def mg_builtin(name, arg_types):
                                 (obj.name, t.__name__, type(a).__name__), ctx=ctx)
             return fn(ctx, *args)
 
+        name = fn.__name__.replace('mgb_', '')
         builtins[name] = BuiltinFunction(name, builtin_call, info=Info('__builtins__', 0))
         return builtin_call
     return annotate
 
 # TODO: error checking!
 
-@mg_builtin('read_file', [String])
+@mg_builtin([String])
 def mgb_read_file(ctx, path):
     with open(path.value) as f:
         return String(f.read(), info=path)
 
-@mg_builtin('putchar', [String])
+@mg_builtin([String])
 def mgb_putchar(ctx, arg):
     sys.stdout.write(arg.value)
     return Nil(info=arg)
 
-@mg_builtin('len', [Node])
+@mg_builtin([Node])
 def mgb_len(ctx, arg):
     return Integer(arg.len(ctx), info=arg)
 
-@mg_builtin('repr', [Node])
+@mg_builtin([Node])
 def mgb_repr(ctx, arg):
     return String(arg.repr(ctx), info=arg)
 
-@mg_builtin('str', [Node])
+@mg_builtin([Node])
 def mgb_str(ctx, arg):
     return String(arg.str(ctx), info=arg)
 
-@mg_builtin('error', [String])
+@mg_builtin([String])
 def mgb_error(ctx, msg):
     msg.error(msg.value, ctx=ctx)
 
-@mg_builtin('slice', [Node, Integer, Integer])
+@mg_builtin([Node, Integer, Integer])
 def mgb_slice(ctx, seq, start, end):
     if isinstance(seq, String):
         return String(seq.value[start.value:end.value], info=seq)
@@ -57,30 +58,26 @@ def mgb_slice(ctx, seq, start, end):
         return List(seq.items[start.value:end.value], info=seq)
     return Nil(info=seq)
 
-@mg_builtin('parse_int', [String, Integer])
+@mg_builtin([String, Integer])
 def mgb_parse_int(ctx, int_str, base):
     return Integer(int(int_str.value, base.value), info=int_str)
 
-@mg_builtin('str_upper', [String])
+@mg_builtin([String])
 def mgb_str_upper(ctx, arg):
     return String(arg.value.upper(), info=arg)
 
-@mg_builtin('re_parse', [String])
-def mgb_str_upper(ctx, arg):
-    return String(arg.value.upper(), info=arg)
-
-@mg_builtin('re_parse', [String])
-def mgb_str_upper(ctx, arg):
+@mg_builtin([String])
+def mgb_re_parse(ctx, arg):
     return String(arg.value.upper(), info=arg)
 
 # Python interfacing functions! At some point these would become a security
 # hole if we cared about the Python interpreter.
-@mg_builtin('py_obj_get', [String])
-def mgb_py_obj(ctx, name):
+@mg_builtin([String])
+def mgb_py_obj_get(ctx, name):
     return PyObject(eval(name.value), info=name)
 
-@mg_builtin('py_obj_call', None)
-def mgb_py_call(ctx, *args):
+@mg_builtin(None)
+def mgb_py_obj_call(ctx, *args):
     fn = args[0]
     assert isinstance(fn, PyObject)
     args = [py_unwrap(a, ctx) for a in args[1:]]
