6057b9b0b2a0432cd0d39acf53a7ed9c145dbf2b
Zach Wegner <zwegner@gmail.com>
2014-05-14 19:56:36 -0500

    Some test.py cleanups

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import sys
+import tempfile
 import time
 
 root_dir = os.path.dirname(sys.path[0])
@@ -35,24 +36,32 @@ for test in mg_tests:
         passes += 1
     except Exception:
         fails += 1
-        print('TEST %s FAILED!' % test)
+        print('MUTAGEN TEST %s FAILED!' % test)
 
 for test in py_tests:
     if test_set and test not in test_set:
         continue
-    # Run python version
-    path = '%s.py' % (test)
-    py_output = subprocess.check_output([python, path], cwd=py_test_dir)
 
-    # Run mutagen version
-    path = '%s.mg' % (test)
-    mg_output = subprocess.check_output([mutagen, path], cwd=py_test_dir)
+    outputs = []
+    for [interpreter, ext] in [[python, 'py'], [mutagen, 'mg']]:
+        path = '%s.%s' % (test, ext)
+        try:
+            output = subprocess.check_output([interpreter, path], cwd=py_test_dir)
+        except subprocess.CalledProcessError:
+            output = b''
+        outputs.append(output)
+    py_output, mg_output = outputs
 
     if py_output != mg_output:
         fails += 1
-        print('TEST %s FAILED!' % test)
-        print('python output:\n%s' % py_output.decode('utf-8'))
-        print('mutagen output:\n%s' % mg_output.decode('utf-8'))
+        print('PYTHON TEST %s FAILED!' % test)
+        with tempfile.NamedTemporaryFile() as f1:
+            f1.write(py_output)
+            f1.flush()
+            with tempfile.NamedTemporaryFile() as f2:
+                f2.write(mg_output)
+                f2.flush()
+                subprocess.call(['diff', '-a', f1.name, f2.name])
     else:
         passes += 1
 
